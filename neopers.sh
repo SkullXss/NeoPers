53c7ce8a10e9bd97e455a3dfe8d3e643830b1b79
Skull'Xss <149721574+SkullXss@users.noreply.github.com>
2023-11-17 16:01:59 -0300

    Update neopers.sh

--- neopers.sh
+++ neopers.sh
@@ -5,35 +5,43 @@ clear
 
 # Banner colorido e animado
 echo -e "\e[1;34m
-  ____                   _              ____ _
- |  _ \  __ _ _ __ __ _| |_ ___  _ __ / ___| | __ _ ___ ___  ___  __ _ ___
- | | | |/ _\` | '__/ _\` | __/ _ \| '__| |   | |/ _\` / __/ __|/ _ \/ _\` / __|
- | |_| | (_| | | | (_| | || (_) | |    |___| | (_| \__ \__ \  __/ (_| \__ \\
- |____/ \__, |_|  \__,_|\__\___/|_|     \____|_|\__,_|___/___/\___|\__,_|___/
-         |___/
+ ____                   _              
+ |  _ \  __ _ _ __ __ _| |_ ___  _ __ 
+ | | | |/ _\` | '__/ _\` | __/ _ \| '__|
+ | |_| | (_| | | | (_| | || (_) | |   
+ |____/ \__, |_|  \__,_|\__\___/|_|   
+         |___/    
 \e[0m"
 
-# Atualiza os repositórios e instala dependências
-echo "Atualizando repositórios e instalando dependências..."
- apt update &&  apt upgrade -y
- apt install neofetch -y
+# Atualiza os repositórios e instala dependências (opcional)
+read -p "Deseja atualizar os repositórios e instalar dependências? (y/n): " resposta
+if [ "$resposta" == "y" ]; then
+    apt update && apt upgrade -y
+    apt install neofetch -y
+    clear
+fi
+echo -e "\e[1;34m
+ ____                   _              
+ |  _ \  __ _ _ __ __ _| |_ ___  _ __ 
+ | | | |/ _\` | '__/ _\` | __/ _ \| '__|
+ | |_| | (_| | | | (_| | || (_) | |   
+ |____/ \__, |_|  \__,_|\__\___/|_|   
+         |___/    
+\e[0m"
 
 # Pergunta pelo nome para o PS1
 read -p "Digite seu nome para ser exibido no prompt (PS1): " nome_usuario
 
-# Pergunta pela senha
-read -s -p "Digite a senha para acessar o Termux: " senha
-echo
-
-# Pergunta pela senha de acesso (caso esqueça a senha principal)
-read -s -p "Digite uma senha de acesso em caso de esquecimento: " senha_acesso
+# Pergunta pela senha de acesso (opcional)
+read -s -p "Digite uma senha de acesso (opcional): " senha_acesso
 echo
 
 # Limpa o conteúdo do ~/.bashrc
 echo "" > ~/.bashrc
 
 # Adiciona as novas configurações
-echo "# Limpa a tela na inicialização
+cat <<EOL >> ~/.bashrc
+# Limpa a tela na inicialização
 clear
 
 # Executa o neofetch na inicialização
@@ -42,42 +50,46 @@ neofetch
 # Configuração do PS1 (prompt)
 PS1='\\[\\033[1;31m\\]╭──\\[\\033[1;30m\\](\\[\\033[1;35m\\]$nome_usuario\\[\\033[1;30m\\]@\\[>\\]
 
-# Verifica a senha de acesso
-read -s -p \"Digite a senha para acessar o Termux: \" senha_digitada
-echo
+# Verifica a senha de acesso (opcional)
+if [ -n "\$senha_acesso" ]; then
+    read -s -p "Digite a senha para acessar o Termux: " senha_digitada
+    echo
 
-if [ \"\$senha_digitada\" != \"$senha\" ]; then
-    echo \"Senha incorreta. Saindo...\"
-    exit 1
+    if [ "\$senha_digitada" != "\$senha_acesso" ]; then
+        echo "Senha incorreta. Saindo..."
+        exit 1
+    fi
 fi
 
 # Configuração das cores para o ls
-export LS_COLORS=\"di=38;5;196:fi=38;5;46\"
+export LS_COLORS="di=38;5;196:fi=38;5;46"
 
 # Alias para o comando ls com formatação amigável
-alias ls=\"ls --color=auto --classify\"
+alias ls="ls --color=auto --classify"
 
 # Alias para o comando ls com emojis indicando pastas e arquivos
-alias lse=\"ls | sed \\\"s/^\\(.*\\//)\\1📁/;s/^\\(.*\\)/\\1📄/\\\"\"
+alias lse="ls | sed 's/^\\(.*\\//)\\1📁/;s/^\\(.*\\)/\\1📄/'"
 
 # Cores para diretórios e arquivos
-LS_COLORS=$LS_COLORS:'di=1;34:'
-LS_COLORS=$LS_COLORS:'*.txt=1;31:'
-LS_COLORS=$LS_COLORS:'*.sh=1;32:'
+LS_COLORS=\$LS_COLORS:'di=1;34:'
+LS_COLORS=\$LS_COLORS:'*.txt=1;31:'
+LS_COLORS=\$LS_COLORS:'*.sh=1;32:'
 export LS_COLORS
 
 # Função para destacar comandos inexistentes
 command_not_found_handle() {
-    echo -e "\[\033[1;31m\]Comando não encontrado: $1\[\033[0m\]"
+    echo -e "\[\033[1;31m\]Comando não encontrado: \$1\[\033[0m\]"
 }
 
 # Funções adicionais
 alias ls='ls --color=auto'
 alias ll='ls -l'
 alias la='ls -a'
-" >> ~/.bashrc
+EOL
 
-# Adiciona a senha de acesso ao arquivo de configuração
-echo "$senha_acesso" > ~/.senha_acesso
+# Adiciona a senha de acesso ao arquivo de configuração (opcional)
+if [ -n "$senha_acesso" ]; then
+    echo "$senha_acesso" > ~/.senha_acesso
+fi
 
 echo "Script executado com sucesso! Seu nome foi configurado para o prompt."
